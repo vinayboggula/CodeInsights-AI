06e541f9642124de78e38f319a06bac3358c8257
vinayboggula <boggulavinay26@gmail.com>
2026-04-09 14:44:34 +0530

    fix: added groq dependency correctly

--- server/server.js
+++ server/server.js
@@ -10,8 +10,9 @@ dotenv.config();
 
 const app = express();
 
+console.log("API URL:", process.env.FRONTEND_URL);
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true
 }));
 app.use(cookieParser());
